ba2cb29ba6e338f9d1f7a691c6be58fb2bdf937b
htikeminswan <htikeminswan2467@gmail.com>
2020-10-15 22:30:25 +0630

    added

---
+++ Test2/Exe11.php
@@ -0,0 +1,28 @@
+<?php 
+
+echo " <center><table border='1'>
+
+			<tbody>
+				<tr>
+					<td>Salary of Mr.A is</td>
+					<td>1000$</td>
+				</tr>
+				<tr>
+					<td>Salary of Mr.B is</td>
+					<td>1200$</td>
+				</tr>
+				<tr>
+					<td>Salary of Mr.C is</td>
+					<td>1400$</td>
+				</tr>
+			</tbody>
+
+
+		</table>";
+
+
+
+
+
+
+?>
---
+++ Test2/Exe12.php
@@ -0,0 +1,13 @@
+<?php
+ $d=array('A001','A002','A003','A004','A005');
+
+//sort($d);
+echo"<center>";
+echo "<br>".$d[0];
+echo "<br>".$d[1];
+echo "<br>".$d[2];
+echo "<br>".$d[3];
+echo "<br>".$d[4];
+
+
+?>
